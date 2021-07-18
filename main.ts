46cd1a53a9eb7f41ca0292518aceaaba0ceb9cc3
cliffwang0825 <87330955+cliffwang0825@users.noreply.github.com>
2021-07-18 23:55:27 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,33 @@
+let ready = false
+input.onPinPressed(TouchPin.P0, function () {
+    if (ready == true) {
+        ready = false
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+        basic.pause(500)
+        basic.clearScreen()
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString("3")
+    basic.pause(100)
+    basic.showString("2")
+    basic.pause(100)
+    basic.showString("1")
+    basic.pause(100)
+    basic.clearScreen()
+    basic.pause(randint(1000, 5000))
+    ready = true
+    basic.showIcon(IconNames.Heart)
+})
 input.onPinPressed(TouchPin.P2, function () {
-    if (game_started == true) {
+    if (ready == true) {
+        ready = false
         basic.showLeds(`
             . . # . .
             . . . # .
@@ -7,44 +35,7 @@ input.onPinPressed(TouchPin.P2, function () {
             . . . # .
             . . # . .
             `)
-        basic.pause(100)
-        game_started = false
-        basic.clearScreen()
-    } else {
-        basic.showIcon(IconNames.Sad)
-    }
-})
-input.onPinPressed(TouchPin.P1, function () {
-    if (game_started == true) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-        basic.pause(100)
-        game_started = false
-        basic.clearScreen()
-    } else {
-        basic.showIcon(IconNames.Sad)
-    }
-})
-let game_started = false
-game_started = false
-basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P0)) {
-        basic.showString("3")
-        basic.pause(100)
-        basic.showString("2")
-        basic.pause(100)
-        basic.showString("1")
-        basic.pause(100)
-        basic.clearScreen()
-        game_started = true
-        basic.pause(randint(1000, 5000))
-        basic.showIcon(IconNames.Heart)
-        basic.pause(50)
+        basic.pause(500)
         basic.clearScreen()
     }
 })
